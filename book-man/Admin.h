1d7194c6bb3d9b1e8c2f4f6fda79e859bd0259fb
EFTIKHAR AZIM <eftikharazim.107308@gmail.com>
2017-12-02 00:47:26 +0600

    added some header file

--- book-man/Admin.h
+++ book-man/Admin.h
@@ -7,9 +7,8 @@ class Admin {
 	string pass;
 public:
 	string name;
-	int type;
-	bool AppAdmin;
 	bool StoreAdmin;
 	bool SetAdmin(string nameIn, string passIn);
+	bool SellApproval(string nameIn, int price);
 	Admin(string name, string pass, int mode);
 };
