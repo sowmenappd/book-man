d5d938b1e3211368e5b53ebb112dcacba323f636
sowmen.appd <sowmen.appd@gmail.com>
2017-12-01 21:27:29 +0600

    defined student.h

---
+++ book-man/Student.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+	string pass;
+	string location;
+	string contact;
+public:
+	string name;
+	string ID;
+	bool buyOrder(Bookstore& bs, Book& bk);
+	bool sellOrder(Book bs, int price);
+	bool SetStudent(string nameIn, string id, string passIn, string locationIn, string contactIn);
+
+};
